08c083b74c08136dcdf90fdb91e9fdbb7011224f
Frank <frankwco@gmail.com>
2024-09-30 22:11:56 -0300

    aula 30/09/2024

---
+++ src/main/java/com/leilao/backend/model/Person.java
@@ -0,0 +1,48 @@
+package com.leilao.backend.model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+@Entity
+@Data
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    @Column(name = "validation_code")
+    private String validationCode;
+    // @Temporal(TemporalType.TIMESTAMP)
+    // private Date validationCodeValidity;
+    private LocalDateTime validationCodeValidity;
+    @OneToMany(mappedBy = "person", orphanRemoval = true,
+     cascade = CascadeType.ALL)
+    @Setter(value = AccessLevel.NONE)
+    private List<PersonProfile> personProfile;
+
+    public void setPersonProfile(List<PersonProfile> lpp) {
+        for (PersonProfile p : lpp) {
+            p.setPerson(this);
+        }
+        personProfile = lpp;
+    }
+}
---
+++ src/main/java/com/leilao/backend/service/PersonService.java
@@ -0,0 +1,31 @@
+package com.leilao.backend.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leilao.backend.model.Person;
+import com.leilao.backend.repository.PersonRepository;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Person create(Person person) {
+        return personRepository.save(person);
+    }
+
+    public Person update(Person person) {
+        Person personSaved = personRepository.findById(person.getId())
+                .orElseThrow(() -> new NoSuchElementException("Objeto não encontrado"));
+
+        personSaved.setName(person.getName());
+        personSaved.setEmail(person.getEmail());
+
+        return personRepository.save(personSaved);
+    }
+}
